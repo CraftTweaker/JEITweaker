c4b416776ac469700a10bacd768d73a6f8b50017
Jared <jaredlll08@gmail.com>
2021-01-27 18:14:33 +0200

    Add /ct dump jeiCategories close #6

--- src/main/java/com/blamejared/jeitweaker/JEIAddonPlugin.java
+++ src/main/java/com/blamejared/jeitweaker/JEIAddonPlugin.java
@@ -4,17 +4,21 @@ import com.blamejared.crafttweaker.api.fluid.IFluidStack;
 import com.blamejared.crafttweaker.api.item.IItemStack;
 import mezz.jei.api.*;
 import mezz.jei.api.ingredients.*;
+import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.registration.*;
 import mezz.jei.api.runtime.*;
 import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.FluidStack;
 
+import java.util.*;
 import java.util.stream.Collectors;
 
 
 @JeiPlugin
 public class JEIAddonPlugin implements IModPlugin {
+    
+    public static final List<ResourceLocation> JEI_CATEGORIES = new ArrayList<>();
     
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
@@ -29,6 +33,7 @@ public class JEIAddonPlugin implements IModPlugin {
     
     @Override
     public void onRuntimeAvailable(IJeiRuntime iJeiRuntime) {
+        
         IIngredientManager ingredientManager = iJeiRuntime.getIngredientManager();
         IIngredientType<ItemStack> itemType = ingredientManager.getIngredientType(ItemStack.class);
         IIngredientType<FluidStack> fluidType = ingredientManager.getIngredientType(FluidStack.class);
@@ -43,6 +48,8 @@ public class JEIAddonPlugin implements IModPlugin {
         JEIManager.HIDDEN_ITEMS.clear();
         JEIManager.HIDDEN_FLUIDS.clear();
         JEIManager.HIDDEN_RECIPE_CATEGORIES.clear();
+        JEI_CATEGORIES.clear();
+        JEI_CATEGORIES.addAll(iJeiRuntime.getRecipeManager().getRecipeCategories().stream().map(IRecipeCategory::getUid).collect(Collectors.toList()));
     }
     
     @Override
--- src/main/java/com/blamejared/jeitweaker/JEITweaker.java
+++ src/main/java/com/blamejared/jeitweaker/JEITweaker.java
@@ -1,10 +1,18 @@
 package com.blamejared.jeitweaker;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("jeitweaker")
 public class JEITweaker {
     
     public JEITweaker() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
+    }
+    
+    private void setupClient(final FMLClientSetupEvent event) {
+        MinecraftForge.EVENT_BUS.register(new Events());
     }
 }
