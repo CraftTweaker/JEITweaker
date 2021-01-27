d1b5681706eeace4d5c194164fb49b89b03b7766
Jared <jaredlll08@gmail.com>
2021-01-27 21:06:59 +0200

    add mod and regex support

--- src/main/java/com/blamejared/jeitweaker/JEIManager.java
+++ src/main/java/com/blamejared/jeitweaker/JEIManager.java
@@ -6,10 +6,15 @@ import com.blamejared.crafttweaker.api.actions.IRuntimeAction;
 import com.blamejared.crafttweaker.api.annotations.ZenRegister;
 import com.blamejared.crafttweaker.api.fluid.IFluidStack;
 import com.blamejared.crafttweaker.api.item.IItemStack;
+import com.blamejared.crafttweaker.api.managers.IRecipeManager;
+import com.blamejared.crafttweaker.impl.item.MCItemStackMutable;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.openzen.zencode.java.ZenCodeType;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 @ZenCodeType.Name("mods.jei.JEI")
 @ZenRegister
@@ -23,7 +28,59 @@ public class JEIManager {
     public static final Map<IItemStack, String[]> ITEM_DESCRIPTIONS = new HashMap<>();
     public static final Map<IFluidStack, String[]> FLUID_DESCRIPTIONS = new HashMap<>();
     
+    @ZenCodeType.Method
+    public static void hideMod(String modid, @ZenCodeType.Optional("(name as string) => {return true;}") IRecipeManager.RecipeFilter exclude) {
+        CraftTweakerAPI.apply(new IRuntimeAction() {
+            @Override
+            public void apply() {
+               ForgeRegistries.ITEMS.getEntries()
+                       .stream()
+                       .filter(entry -> entry.getKey().getLocation().getNamespace().equalsIgnoreCase(modid))
+                       .filter(entry -> !exclude.test(entry.getKey().getLocation().getPath()))
+                       .map(registryKeyItemEntry -> new ItemStack(registryKeyItemEntry.getValue()))
+                       .filter(itemStack -> !itemStack.isEmpty())
+                       .forEach(stack -> HIDDEN_ITEMS.add(new MCItemStackMutable(stack)));
+                
+            }
+            
+            @Override
+            public String describe() {
+                return "JEI Hiding all Items from Mod: " + modid;
+            }
+            
+            @Override
+            public boolean shouldApplyOn(LogicalSide side) {
+                return !CraftTweaker.serverOverride && side.isClient();
+            }
+        });
+    }
     
+    @ZenCodeType.Method
+    public static void hideRegex(String regex) {
+        Pattern compile = Pattern.compile(regex);
+        CraftTweakerAPI.apply(new IRuntimeAction() {
+            @Override
+            public void apply() {
+                ForgeRegistries.ITEMS.getEntries()
+                        .stream()
+                        .filter(entry -> compile.matcher(entry.getKey().getLocation().toString()).matches())
+                        .map(registryKeyItemEntry -> new ItemStack(registryKeyItemEntry.getValue()))
+                        .filter(itemStack -> !itemStack.isEmpty())
+                        .forEach(stack -> HIDDEN_ITEMS.add(new MCItemStackMutable(stack)));
+                
+            }
+            
+            @Override
+            public String describe() {
+                return "JEI Hiding all Items from by Regex: " + regex;
+            }
+            
+            @Override
+            public boolean shouldApplyOn(LogicalSide side) {
+                return !CraftTweaker.serverOverride && side.isClient();
+            }
+        });
+    }
     
     @ZenCodeType.Method
     public static void hideItem(IItemStack stack) {
