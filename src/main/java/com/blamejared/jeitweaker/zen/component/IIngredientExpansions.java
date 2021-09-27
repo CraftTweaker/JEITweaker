47e6083a0caadddbe967b90b2a8ea137e354ee90
TheSilkMiner <thesilkminer@outlook.com>
2021-09-27 10:44:19 +0200

    Allow conversion of IIngredients to JEI ingredients
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/java/com/blamejared/jeitweaker/zen/component/IIngredientExpansions.java
@@ -0,0 +1,21 @@
+package com.blamejared.jeitweaker.zen.component;
+
+import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+import com.blamejared.crafttweaker.api.item.IIngredient;
+import org.openzen.zencode.java.ZenCodeType;
+
+import java.util.Arrays;
+
+//@Document("mods/JEITweaker/Component/IIngredientExpansions")
+@ZenCodeType.Expansion("crafttweaker.api.item.IIngredient")
+@ZenRegister
+public final class IIngredientExpansions {
+    
+    @ZenCodeType.Caster(implicit = true)
+    public static HackyJeiIngredientToMakeZenCodeHappy[] asJeiIngredientArray(final IIngredient ingredient) {
+        
+        return Arrays.stream(ingredient.getItems())
+                .map(IItemStackExpansions::asJeiIngredient)
+                .toArray(HackyJeiIngredientToMakeZenCodeHappy[]::new);
+    }
+}
