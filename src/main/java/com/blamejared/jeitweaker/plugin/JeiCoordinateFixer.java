966e0dec5b78334ad2f16a8fe5d85090b201e3f7
TheSilkMiner <thesilkminer@outlook.com>
2021-09-28 11:15:38 +0200

    Allow registration of additional fixers
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/blamejared/jeitweaker/plugin/JeiCoordinateFixer.java
+++ src/main/java/com/blamejared/jeitweaker/plugin/JeiCoordinateFixer.java
@@ -4,7 +4,6 @@ import com.google.common.base.Suppliers;
 import com.mojang.datafixers.util.Pair;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.runtime.IIngredientManager;
-import net.minecraft.util.Util;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,16 +14,25 @@ import java.util.stream.Collectors;
 public final class JeiCoordinateFixer {
     
     private static final IntUnaryOperator IDENTITY = it -> it;
-    private static final Map<JeiTweakerIngredientType<?, ?>, IntUnaryOperator> RAW_FIXERS = Util.make(new HashMap<>(), map -> {
-        
-        map.put(JeiTweakerIngredientType.ITEM, it -> it - 1);
-    });
+    private static final Map<JeiTweakerIngredientType<?, ?>, IntUnaryOperator> RAW_FIXERS = new HashMap<>();
+    
+    static {
+        // TODO("This is a temporary location: find where best to place this")
+        registerRawFixer(JeiTweakerIngredientType.ITEM, it -> it - 1);
+    }
     
     private final Supplier<Map<IIngredientType<?>, IntUnaryOperator>> fixers;
     
     JeiCoordinateFixer(final IIngredientManager manager) {
     
         this.fixers = Suppliers.memoize(() -> this.computeFixers(manager));
+    }
+    
+    public static <T, U> void registerRawFixer(final JeiTweakerIngredientType<T, U> type, final IntUnaryOperator fixer) {
+        
+        if (RAW_FIXERS.containsKey(type)) return;
+        
+        RAW_FIXERS.put(type, fixer);
     }
     
     public IntUnaryOperator findFor(final IIngredientType<?> type) {
