6e3f31c56f6a6c41a07f28f953529b2f6beb4671
TheSilkMiner <thesilkminer@outlook.com>
2021-09-28 18:23:00 +0200

    Allow item tags to automatically cast to ingredients
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/java/com/blamejared/jeitweaker/zen/component/ItemTagExpansions.java
@@ -0,0 +1,20 @@
+package com.blamejared.jeitweaker.zen.component;
+
+import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+import com.blamejared.crafttweaker.impl.tag.MCTag;
+import com.blamejared.crafttweaker.impl.tag.expansions.ExpandItemTag;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+import net.minecraft.item.Item;
+import org.openzen.zencode.java.ZenCodeType;
+
+//@Document("mods/JEITweaker/Component/ItemTagExpansions")
+@ZenCodeType.Expansion("crafttweaker.api.tag.MCTag<crafttweaker.api.item.MCItemDefinition>")
+@ZenRegister
+public final class ItemTagExpansions {
+    
+    @ZenCodeType.Caster(implicit = true)
+    public static HackyJeiIngredientToMakeZenCodeHappy[] asJeiIngredientArray(final MCTag<Item> tag) {
+        
+        return IIngredientExpansions.asJeiIngredientArray(ExpandItemTag.asIIngredient(tag));
+    }
+}
