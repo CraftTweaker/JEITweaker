b29f722b73acc925d6d3470cabfe94ccab593c7b
TheSilkMiner <thesilkminer@outlook.com>
2023-02-07 11:51:22 +0100

    Store JeiT logger
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- common/src/main/java/com/blamejared/jeitweaker/common/JeiTweakerInitializer.java
+++ common/src/main/java/com/blamejared/jeitweaker/common/JeiTweakerInitializer.java
@@ -1,17 +1,21 @@
 package com.blamejared.jeitweaker.common;
 
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.jeitweaker.common.command.CommandManager;
+import org.apache.logging.log4j.Logger;
 
 public final class JeiTweakerInitializer {
     private static final JeiTweakerInitializer INSTANCE = new JeiTweakerInitializer();
     
     private final CommandManager manager;
+    private final Logger jeiTweakerLogger;
     
     private boolean init;
     
     private JeiTweakerInitializer() {
         this.manager = CommandManager.of();
         this.init = false;
+        this.jeiTweakerLogger = CraftTweakerAPI.LOGGER; // Preparing for 1.19.3 :P
     }
     
     public static JeiTweakerInitializer get() {
@@ -26,6 +30,11 @@ public final class JeiTweakerInitializer {
     public CommandManager commandManager() {
         this.assertInit();
         return this.manager;
+    }
+    
+    public Logger jeiTweakerLogger() {
+        this.assertInit();
+        return this.jeiTweakerLogger;
     }
     
     private void assertInit() {
