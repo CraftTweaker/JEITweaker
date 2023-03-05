9b92b3f08837953065c96dd7ac93399da91b5fec
TheSilkMiner <thesilkminer@outlook.com>
2023-03-05 13:41:16 +0100

    Fix missing equals and hashcode in JeiIngredient impls
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- common/src/main/java/com/blamejared/jeitweaker/common/ingredient/SimpleJeiIngredient.java
+++ common/src/main/java/com/blamejared/jeitweaker/common/ingredient/SimpleJeiIngredient.java
@@ -24,6 +24,35 @@ abstract sealed class SimpleJeiIngredient<J, Z> implements JeiIngredient<J, Z> p
     }
     
     @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof JeiIngredient<?, ?> ingredient)) {
+            return false;
+        }
+        // Note we require only either the jei content or the zen content to be the same. This is because ingredients
+        // need not store both types, meaning that one might be created from the other. In case that one of the types
+        // does not properly implement equals (i.e. relies on identity), but the other is in fact equal according to
+        // some definition, we want to declare the two ingredients as equal. Essentially, one of the two getters might
+        // be computing a new object every time that relies on identity for equality, whereas the other implements
+        // equals properly.
+        return Objects.equals(this.type(), ingredient.type()) &&
+                (Objects.equals(this.jeiContent(), ingredient.jeiContent()) || Objects.equals(this.zenContent(), ingredient.zenContent()));
+    }
+    
+    @Override
+    public int hashCode() {
+        // According to equals, this is the only object that can remain constant and, since we do not know which one
+        // of the two objects is equal and #hashCode should be independent of #equals, this is the only realistic
+        // approach we can take.
+        return Objects.hashCode(this.type());
+    }
+    
+    @Override
     public final String toString() {
         return this.converter().toCommandStringFromJei(this.jeiContent());
     }
