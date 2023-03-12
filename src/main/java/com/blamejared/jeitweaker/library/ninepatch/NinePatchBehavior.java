a655dbbf57ca0b6e88b4bc4da9ec5002b4d3d11a
Jared <jaredlll08@gmail.com>
2023-03-12 22:35:33 +0200

    remove old src folder

--- src/main/java/com/blamejared/jeitweaker/library/ninepatch/NinePatchBehavior.java
+++
@@ -1,6 +0,0 @@
-package com.blamejared.jeitweaker.library.ninepatch;
-
-enum NinePatchBehavior {
-    STRETCH,
-    TILE
-}
--- src/main/java/com/blamejared/jeitweaker/library/ninepatch/package-info.java
+++
@@ -1,4 +0,0 @@
-/*
- * Temporary: it will be fully fledged out in 1.17+
- */
-package com.blamejared.jeitweaker.library.ninepatch;
